7d833858adff38f4ccf784aac53b1051bec9171e
Yi Qingliang <niqingliang2003@gmail.com>
2016-11-18 10:27:42 +0000

    add scripts
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+/mnt/xilinx/Vivado/2016.3/bin/vivado -mode batch -source build.tcl
---
+++ build.tcl
@@ -0,0 +1,2 @@
+create_project fsref . -part xc7z020clg400-1
+set_property simulator_language Verilog [current_project]
