f64e56eb4f1ffc284ed8f3664c231e51ad274b73
Yi Qingliang <niqingliang2003@gmail.com>
2017-08-22 19:17:05 +0800

    split axilite2regctl from 'fsctl'
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- ip/fsctl/generate.tcl
+++ ip/fsctl/generate.tcl
@@ -9,8 +9,8 @@ ipx::edit_ip_in_project -upgrade true -name edit_ip_project -directory $tmp_dir 
 ipx::current_core $ip_dir/component.xml
 
 pip_set_prop [ipx::current_core] [subst {
-	display_name {FsCtl}
-	description {Fusion Splice Controller}
+	display_name {Fusion Splicer Controller}
+	description {Fusion Splicer Controller}
 	vendor_display_name $VENDORDISPNAME
 	version $VERSION
 	company_url $COMPANYURL
@@ -19,58 +19,88 @@ pip_set_prop [ipx::current_core] [subst {
 
 pip_clr_def_if_par_memmap [ipx::current_core]
 
-pip_add_bus_if [ipx::current_core] S_AXI_LITE {
-	abstraction_type_vlnv {xilinx.com:interface:aximm_rtl:1.0}
-	bus_type_vlnv {xilinx.com:interface:aximm:1.0}
+pip_add_bus_if [ipx::current_core] REG_CTL [subst {
+	abstraction_type_vlnv {$VENDOR:interface:reg_ctl_rtl:1.0}
+	bus_type_vlnv {$VENDOR:interface:reg_ctl:1.0}
 	interface_mode {slave}
-} {
-	AWADDR	s_axi_awaddr
-	AWPROT	s_axi_awprot
-	AWVALID	s_axi_awvalid
-	AWREADY	s_axi_awready
-	WDATA	s_axi_wdata
-	WVALID	s_axi_wvalid
-	WREADY	s_axi_wready
-	BRESP	s_axi_bresp
-	BVALID	s_axi_bvalid
-	BREADY	s_axi_bready
+}] {
+	RD_ADDR rd_addr
+	RD_DATA rd_data
+	WR_EN   wr_en
+	WR_ADDR wr_addr
+	WR_DATA wr_data
+}
 
-	ARADDR	s_axi_araddr
-	ARPROT	s_axi_arprot
-	ARVALID	s_axi_arvalid
-	ARREADY	s_axi_arready
-	RDATA	s_axi_rdata
-	RRESP	s_axi_rresp
-	RVALID	s_axi_rvalid
-	RREADY	s_axi_rready
+pip_add_bus_if [ipx::current_core] OUT_SIZE [subst {
+	abstraction_type_vlnv {$VENDOR:interface:window_ctl_rtl:1.0}
+	bus_type_vlnv {$VENDOR:interface:window_ctl:1.0}
+	interface_mode {master}
+}] [subst {
+	WIDTH  out_width
+	HEIGHT out_height
+}]
+
+for {set i 0} {$i < 3} {incr i} {
+	pip_add_bus_if [ipx::current_core] S[set i]_WIN [subst {
+		abstraction_type_vlnv {$VENDOR:interface:window_ctl_rtl:1.0}
+		bus_type_vlnv {$VENDOR:interface:window_ctl:1.0}
+		interface_mode {master}
+	}] [subst {
+		LEFT   s[set i]_win_left
+		WIDTH  s[set i]_win_width
+		TOP    s[set i]_win_top
+		HEIGHT s[set i]_win_height
+	}]
+
+	pip_add_bus_if [ipx::current_core] S[set i]_SCALE [subst {
+		abstraction_type_vlnv {$VENDOR:interface:window_ctl_rtl:1.0}
+		bus_type_vlnv {$VENDOR:interface:window_ctl:1.0}
+		interface_mode {master}
+	}] [subst {
+		SRC_WIDTH  s[set i]_scale_src_width
+		SRC_HEIGHT s[set i]_scale_src_height
+		DST_WIDTH  s[set i]_scale_dst_width
+		DST_HEIGHT s[set i]_scale_dst_height
+	}]
+
+	pip_add_bus_if [ipx::current_core] S[set i]_DST [subst {
+		abstraction_type_vlnv {$VENDOR:interface:window_ctl_rtl:1.0}
+		bus_type_vlnv {$VENDOR:interface:window_ctl:1.0}
+		interface_mode {master}
+	}] [subst {
+		LEFT   s[set i]_dst_left
+		WIDTH  s[set i]_dst_width
+		TOP    s[set i]_dst_top
+		HEIGHT s[set i]_dst_height
+	}]
 }
 
 # clock & reset
-pip_add_bus_if [ipx::current_core] aresetn {
+pip_add_bus_if [ipx::current_core] resetn {
 	abstraction_type_vlnv xilinx.com:signal:reset_rtl:1.0
 	bus_type_vlnv xilinx.com:signal:reset:1.0
 	interface_mode slave
 } {
-	RST aresetn
+	RST resetn
 } {
 	POLARITY {ACTIVE_LOW}
 }
 
-pip_add_bus_if [ipx::current_core] aclk {
+pip_add_bus_if [ipx::current_core] clk {
 	abstraction_type_vlnv xilinx.com:signal:clock_rtl:1.0
 	bus_type_vlnv xilinx.com:signal:clock:1.0
 	interface_mode slave
 } {
-	CLK aclk
+	CLK clk
 } {
-	ASSOCIATED_BUSIF {S_AXI_LITE}
-	ASSOCIATED_RESET {aresetn}
+	ASSOCIATED_BUSIF {REG_CTL}
+	ASSOCIATED_RESET {resetn}
 }
 
 # parameters
-pip_add_usr_par [ipx::current_core] {C_S_AXI_DATA_WIDTH} {
-	display_name {S_AXI_LITE Data Width}
-	tooltip {S_AXI_LITE DATA WIDTH}
+pip_add_usr_par [ipx::current_core] {C_DATA_WIDTH} {
+	display_name {Data Width}
+	tooltip { DATA WIDTH}
 	widget {comboBox}
 } {
 	value_resolve_type user
@@ -97,12 +127,59 @@ pip_add_usr_par [ipx::current_core] {C_ADDR_WIDTH} {
 	value 8
 	value_format long
 }
+pip_add_usr_par [ipx::current_core] {C_IMG_WBITS} {
+	display_name {Image Width (PIXEL) Bit Width}
+	tooltip {IMAGE WIDTH BIT WIDTH}
+	widget {comboBox}
+} {
+	value_resolve_type user
+	value 12
+	value_format long
+	value_validation_type list
+	value_validation_list {5 6 7 8 9 10 11 12}
+} {
+	value 12
+	value_format long
+}
 
-# address space
-pip_add_memory_map [ipx::current_core] S_AXI_LITE S_AXI_LITE_reg S_AXI_LITE {
-	width 32
-	range 4096
-	usage register
+pip_add_usr_par [ipx::current_core] {C_IMG_HBITS} {
+	display_name {Image Height (PIXEL) Bit Width}
+	tooltip {IMAGE HEIGHT BIT WIDTH}
+	widget {comboBox}
+} {
+	value_resolve_type user
+	value 12
+	value_format long
+	value_validation_type list
+	value_validation_list {5 6 7 8 9 10 11 12}
+} {
+	value 12
+	value_format long
+}
+pip_add_usr_par [ipx::current_core] {C_IMG_WDEF} {
+	display_name {Default Image Width}
+	tooltip {Default Image Width}
+	widget {textEdit}
+} {
+	value_resolve_type user
+	value 320
+	value_format long
+} {
+	value 320
+	value_format long
+}
+
+pip_add_usr_par [ipx::current_core] {C_IMG_HDEF} {
+	display_name {Default Image Height}
+	tooltip {Default Image Height}
+	widget {textEdit}
+} {
+	value_resolve_type user
+	value 240
+	value_format long
+} {
+	value 240
+	value_format long
 }
 
 ipx::create_xgui_files [ipx::current_core]
