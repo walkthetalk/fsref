7c4431b584b6b4035ce0e1b7805fa5a7c98d7b04
Yi Qingliang <niqingliang2003@gmail.com>
2017-08-25 08:27:25 +0800

    ip:axis_window: fix bug
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- ip/axis_window/src/axis_window.v
+++ ip/axis_window/src/axis_window.v
@@ -44,20 +44,29 @@ module axis_window #
 	/// M_AXIS
 	output wire  m_axis_tvalid,
 	output reg  [C_PIXEL_WIDTH-1:0] m_axis_tdata,
-	output reg  m_axis_tuser,
-	output reg  m_axis_tlast,
-	input  wire m_axis_tready
+	output wire  m_axis_tuser,
+	output wire  m_axis_tlast,
+	input  wire  m_axis_tready
 );
 
 	reg [C_IMG_WBITS-1 : 0] col_idx;
 	reg [C_IMG_HBITS-1 : 0] row_idx;
+	reg dlast;
 	reg [C_PIXEL_WIDTH-1:0] data;
 	reg dvalid;
-	wire dready;
 
 	wire nextin;
 	assign nextin = s_axis_tvalid & s_axis_tready;
-	assign s_axis_tready = ~dvalid | dready;
+	assign s_axis_tready = ~m_axis_tvalid | m_axis_tready;
+
+	always @ (posedge clk) begin
+		if (resetn == 1'b0)
+			dlast <= 0;
+		else if (nextin)
+			dlast <= s_axis_tlast;
+		else
+			dlast <= dlast;
+	end
 
 	always @ (posedge clk) begin
 		if (resetn == 1'b0) begin
@@ -69,7 +78,7 @@ module axis_window #
 				col_idx <= 0;
 				row_idx <= 0;
 			end
-			else if (s_axis_tlast) begin
+			else if (dlast) begin
 				col_idx <= 0;
 				row_idx <= row_idx + 1;
 			end
@@ -85,11 +94,11 @@ module axis_window #
 	end
 	always @(posedge clk) begin
 		if (resetn == 1'b0)
-			data <= 0;
+			m_axis_tdata <= 0;
 		else if (nextin)
-			data <= s_axis_tdata;
+			m_axis_tdata <= s_axis_tdata;
 		else
-			data <= data;
+			m_axis_tdata <= m_axis_tdata;
 	end
 
 	always @ (posedge clk) begin
@@ -97,77 +106,19 @@ module axis_window #
 			dvalid <= 0;
 		else if (nextin)
 			dvalid <= 1;
-		else if (dready)
+		else if (m_axis_tready)
 			dvalid <= 0;
 		else
 			dvalid <= dvalid;
 	end
 
-	reg col_valid;
-	reg row_valid;
-	reg dvalid_d1;
-	assign m_axis_tvalid = col_valid & row_valid & dvalid_d1;
-	wire nextin_d1;
-	assign nextin_d1 = dvalid & dready;
-	assign dready = ~m_axis_tvalid | m_axis_tready;
-	always @ (posedge clk) begin
-		if (resetn == 1'b0)
-			col_valid <= 0;
-		else if (nextin_d1) begin
-			if (win_left <= col_idx && col_idx <= win_left + win_width)
-				col_valid <= 1;
-			else
-				col_valid <= 0;
-		end
-		else
-			col_valid <= col_valid;
-	end
-	always @ (posedge clk) begin
-		if (resetn == 1'b0)
-			row_valid <= 0;
-		else if (nextin_d1) begin
-			if (win_top <= row_idx && row_idx <= win_top + win_height)
-				row_valid <= 1;
-			else
-				row_valid <= row_valid;
-		end
-		else
-			row_valid <= row_valid;
-	end
-	always @ (posedge clk) begin
-		if (resetn == 1'b0)
-			dvalid_d1 <= 1'b0;
-		else if (nextin_d1)
-			dvalid_d1 <= 1'b1;
-		else if (m_axis_tready)
-			dvalid_d1 <= 1'b0;
-		else
-			dvalid_d1 <= dvalid_d1;
-	end
-	always @(posedge clk) begin
-		if (resetn == 1'b0)
-			m_axis_tlast <= 1'b0;
-		else if (nextin_d1)
-			m_axis_tlast <= (col_idx == win_left + win_width);
-		else
-			m_axis_tlast <= m_axis_tlast;
-	end
-	always @(posedge clk) begin
-		if (resetn == 1'b0)
-			m_axis_tuser <= 1'b0;
-		else if (nextin_d1)
-			m_axis_tuser <= (col_idx == win_left && row_idx == win_top);
-		else
-			m_axis_tuser <= m_axis_tuser;
-	end
+	wire col_valid;
+	assign col_valid = (win_left <= col_idx && col_idx < win_left + win_width);
+	wire row_valid;
+	assign row_valid = (win_top <= row_idx && row_idx < win_top + win_height);
+	assign m_axis_tvalid = col_valid & row_valid & dvalid;
 
-	always @ (posedge clk) begin
-		if (resetn == 1'b0)
-			m_axis_tdata <= 0;
-		else if (nextin_d1)
-			m_axis_tdata <= data;
-		else
-			m_axis_tdata <= m_axis_tdata;
-	end
+	assign m_axis_tlast = (col_idx == win_left + win_width - 1);
+	assign m_axis_tuser = (col_idx == win_left && row_idx == win_top);
 
 endmodule
