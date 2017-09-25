3ff387e75d72fea47c0201757881ddb93afd9cf1
Yi Qingliang <niqingliang2003@gmail.com>
2017-09-25 14:02:05 +0800

    add new ip - axis_relay (for timing issue)
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

---
+++ ip/axis_relay/src/axis_relay.v
@@ -0,0 +1,124 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 11/18/2016 01:33:37 PM
+// Design Name:
+// Module Name: scaler
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module axis_relay #
+(
+	parameter integer C_PIXEL_WIDTH = 8
+) (
+	input  wire clk,
+	input  wire resetn,
+
+	input  wire s_axis_tvalid,
+	input  wire [C_PIXEL_WIDTH-1:0] s_axis_tdata,
+	input  wire s_axis_tuser,
+	input  wire s_axis_tlast,
+	output reg  s_axis_tready,
+
+	output wire  m_axis_tvalid,
+	output wire  [C_PIXEL_WIDTH-1:0] m_axis_tdata,
+	output wire  m_axis_tuser,
+	output wire  m_axis_tlast,
+	input  wire m_axis_tready
+);
+
+	reg                     relay_tvalid[1:0];
+	reg [C_PIXEL_WIDTH-1:0] relay_tdata[1:0];
+	reg                     relay_tuser[1:0];
+	reg                     relay_tlast[1:0];
+
+	wire snext;
+	assign snext = s_axis_tvalid && s_axis_tready;
+	wire mnext;
+	assign mnext = m_axis_tvalid && m_axis_tready;
+	assign m_axis_tvalid = relay_tvalid[0];
+	assign m_axis_tdata = relay_tdata[0];
+	assign m_axis_tlast = relay_tlast[0];
+	assign m_axis_tuser = relay_tuser[0];
+
+	always @ (posedge clk) begin
+		if (resetn == 1'b0) begin
+			relay_tvalid[1] <= 0;
+			relay_tdata[1] <= 0;
+			relay_tuser[1] <= 0;
+			relay_tlast[1] <= 0;
+		end
+		else if (snext) begin
+			if (relay_tvalid[1]) begin
+				relay_tvalid[1] <= 1;
+				relay_tdata[1] <= s_axis_tdata;
+				relay_tuser[1] <= s_axis_tuser;
+				relay_tlast[1] <= s_axis_tlast;
+			end
+			else if (relay_tvalid[0] && ~m_axis_tready) begin
+				relay_tvalid[1] <= 1;
+				relay_tdata[1] <= s_axis_tdata;
+				relay_tuser[1] <= s_axis_tuser;
+				relay_tlast[1] <= s_axis_tlast;
+			end
+			else
+				relay_tvalid[1] <= 0;
+		end
+		else if (~relay_tvalid[0] || m_axis_tready) begin
+			relay_tvalid[1] <= 0;
+		end
+	end
+
+	always @ (posedge clk) begin
+		if (resetn == 1'b0) begin
+			relay_tvalid[0] <= 0;
+			relay_tdata[0] <= 0;
+			relay_tuser[0] <= 0;
+			relay_tlast[0] <= 0;
+		end
+		else if (~relay_tvalid[0] || m_axis_tready) begin
+			if (relay_tvalid[1]) begin
+				relay_tvalid[0] <= 1;
+				relay_tdata[0] <= relay_tdata[1];
+				relay_tuser[0] <= relay_tuser[1];
+				relay_tlast[0] <= relay_tlast[1];
+			end
+			else if (snext) begin
+				relay_tvalid[0] <= 1;
+				relay_tdata[0] <= s_axis_tdata;
+				relay_tuser[0] <= s_axis_tuser;
+				relay_tlast[0] <= s_axis_tlast;
+			end
+			else begin
+				relay_tvalid[0] <= 0;
+			end
+		end
+	end
+
+	always @ (posedge clk) begin
+		if (resetn == 1'b0)
+			s_axis_tready <= 0;
+		else begin
+			case ({relay_tvalid[1], relay_tvalid[0]})
+			2'b00, 2'b10:
+				s_axis_tready <= 1;
+			2'b01:
+				s_axis_tready <= m_axis_tready;
+			2'b11:
+				s_axis_tready <= 0;
+			endcase
+		end
+	end
+
+endmodule
---
+++ ip/axis_relay/test/test.v
@@ -0,0 +1,158 @@
+`timescale 1ns / 1ps
+`include "../src/axis_relay.v"
+module testrelay(
+    );
+
+    localparam RANDOMOUTPUT = 1;
+    localparam RANDOMINPUT = 0;
+
+	wire[7:0] mdata;
+	wire mlast;
+	reg mready;
+	wire muser;
+	wire mvalid;
+
+	wire[7:0] sdata;
+	wire slast;
+	wire sready;
+	wire suser;
+	reg svalid;
+
+	reg[11:0] ori_height = 10;
+	reg[11:0] ori_width = 10;
+
+	reg resetn;
+	reg clk;
+
+axis_relay #(
+	.C_PIXEL_WIDTH(8)
+)uut(
+	.m_axis_tdata(mdata),
+	.m_axis_tlast(mlast),
+	.m_axis_tready(mready),
+	.m_axis_tuser(muser),
+	.m_axis_tvalid(mvalid),
+	.s_axis_tdata(sdata),
+	.s_axis_tlast(slast),
+	.s_axis_tready(sready),
+	.s_axis_tuser(suser),
+	.s_axis_tvalid(svalid),
+	.clk(clk),
+	.resetn(resetn));
+
+initial begin
+	clk <= 1'b1;
+	forever #1 clk <= ~clk;
+end
+initial begin
+	resetn <= 1'b0;
+	repeat (5) #2 resetn <= 1'b0;
+	forever #2 resetn <= 1'b1;
+end
+
+reg[11:0] in_row;
+reg[11:0] in_col;
+assign sdata = (in_row * 16 + in_col);
+
+reg[11:0] out_row;
+reg[11:0] out_col;
+reg[7:0]  out_data;
+reg out_last;
+
+assign suser = (in_row == 0 && in_col == 0);
+assign slast = (in_col == ori_height - 1);
+
+reg randominput;
+reg randomoutput;
+always @(posedge clk) begin
+	if (resetn == 1'b0)
+		randominput <= 1'b0;
+	else
+		randominput <= (RANDOMINPUT ? {$random}%2 : 1);
+
+	if (resetn == 1'b0)
+		randomoutput <= 1'b0;
+	else
+		randomoutput <= (RANDOMOUTPUT ? {$random}%2 : 1);
+
+	if (resetn == 1'b0) begin
+		in_row <= 0;
+		in_col <= 0;
+	end
+	else if (svalid && sready) begin
+		if (in_col != ori_width - 1) begin
+			in_col <= in_col + 1;
+			in_row <= in_row;
+		end
+		else if (in_row != ori_height - 1) begin
+			in_col <= 0;
+			in_row <= in_row + 1;
+		end
+		else begin
+			in_row <= 0;
+			in_col <= 0;
+		end
+	end
+
+	if (resetn == 1'b0)
+		svalid <= 1'b0;
+	else if (~svalid) begin
+		if (randominput) begin
+			svalid <= 1'b1;
+		end
+	end
+	else if (svalid && sready)  begin
+		if (randominput) begin
+			svalid <= 1'b1;
+		end
+		else begin
+			svalid <= 1'b0;
+		end
+	end
+
+
+	if (resetn == 1'b0)
+		mready <= 1'b0;
+	else if (randomoutput)
+		mready <= 1'b1;
+	else
+		mready <= 1'b0;
+
+
+	if (resetn == 1'b0) begin
+		out_data <= 0;
+		out_last <= 0;
+	end
+	else if (mready && mvalid) begin
+		out_data <= mdata;
+		out_last <= mlast;
+	end
+
+
+	if (resetn == 1'b0) begin
+		out_col = 0;
+		out_row = 0;
+	end
+	else if (mready && mvalid) begin
+		if (muser) begin
+			out_col = 0;
+			out_row = 0;
+		end
+		else if (out_last) begin
+			out_col = 0;
+			out_row = out_row + 1;
+		end
+		else begin
+			out_col = out_col + 1;
+			out_row = out_row;
+		end
+		if (muser)
+			$write("start new frame: \n");
+		$write("%h ", mdata);
+		if (mlast)
+			$write("\n");
+	end
+end
+
+
+endmodule
