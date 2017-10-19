d846ce51220b18694d2dcd54f0fa78b321876a60
Yi Qingliang <niqingliang2003@gmail.com>
2017-10-19 17:57:33 +0800

    step_motor: time-division block-ram
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- ip/step_motor/src/step_motor.v
+++ ip/step_motor/src/step_motor.v
@@ -1,8 +1,11 @@
 module block_ram #(
-	parameter C_STEP_NUMBER_WIDTH = 16,
-	parameter C_SPEED_DATA_WIDTH = 16,
-	parameter C_SPEED_ADDRESS_WIDTH = 9,
-	parameter C_CLK_DIV_BITS = 5
+	parameter integer C_BRAM_ADDR_WIDTH = 9,
+	parameter integer C_STEP_NUMBER_WIDTH = 16,
+	parameter integer C_SPEED_DATA_WIDTH = 16,
+	parameter integer C_SPEED_ADDRESS_WIDTH = 9,
+	parameter integer C_CLK_DIV_NBR = 32,	/// >= 4
+	parameter integer C_MOTOR_NBR = 4
+
 )(
 	input	clk,
 	input	resetn,
@@ -11,34 +14,128 @@ module block_ram #(
 
 	output	o_interrupt,
 
-	output	o_drive,
-	output	o_dir,
-	output	o_ms0,
-	output	o_ms1,
-	output	o_xen,
-	output	o_xrst,
+	output wire o_drive,
+	output wire o_dir,
+	output wire o_ms0,
+	output wire o_ms1,
+	output wire o_xen,
+	output wire o_xrst,
 
-	input[C_SPEED_DATA_WIDTH-1:0]	i_velocity,
-	input[C_STEP_NUMBER_WIDTH-1:0]	i_step_nbr,
-	input		i_start,
-	input		i_stop,
-	input		i_dir,
-	input		i_en,
-	input		i_rst,
-	input[1:0]	i_ms
+	input wire [C_SPEED_DATA_WIDTH-1:0]	i_velocity,
+	input wire [C_STEP_NUMBER_WIDTH-1:0]	i_step_nbr,
+	input wire i_start,
+	input wire i_stop,
+	input wire i_dir,
+	input wire i_en,
+	input wire i_rst,
+	input wire [1:0] i_ms
 );
+	function integer clogb2(input integer bit_depth); begin
+		for(clogb2=0; bit_depth>0; clogb2=clogb2+1)
+			bit_depth = bit_depth>>1;
+		end
+	endfunction
 
-	reg [C_CLK_DIV_BITS-1:0] clk_div_cnt;
-	reg clk_en;
+	//localparam integer C_CLK_DIV_BITS = clogb2(C_CLK_DIV_NBR);
+	//localparam integer C_CLK_DIV_NBR = 2**C_CLK_DIV_BITS;
+	reg [C_CLK_DIV_NBR-1:0] clk_en;
 	always @ (posedge clk) begin
 		if (resetn == 1'b0) begin
-			clk_div_cnt <= 0;
-			clk_en <= 0;
+			clk_en <= 1;
 		end
 		else begin
-			clk_div_cnt <= clk_div_cnt + 1;
-			clk_en <= (clk_div_cnt == 0);
+			clk_en <= {clk_en[C_CLK_DIV_NBR-2:0], clk_en[C_CLK_DIV_NBR-1]};
 		end
 	end
+
+	reg [C_MOTOR_NBR-1:0] rd_en;
+	reg [C_MOTOR_NBR-1:0] rd_en_d1;
+	reg [C_MOTOR_NBR-1:0] rd_en_d2;
+	reg [C_BRAM_ADDR_WIDTH-1:0] rd_addr[C_MOTOR_NBR-1:0];
+	reg [C_SPEED_DATA_WIDTH-1:0] speed[C_MOTOR_NBR-1:0];
+	wire [C_MOTOR_NBR-1:0] rd_addr_conv[C_BRAM_ADDR_WIDTH-1:0];
+	generate
+		genvar i, j;
+		for (j=0; j < C_MOTOR_NBR; j = j + 1) begin: single_motor_conv
+			for (i=0; i < C_BRAM_ADDR_WIDTH; i = i + 1) begin: single_addr_bit_conv
+				assign rd_addr_conv[i][j] = rd_addr[j][i];
+			end
+			always @ (posedge clk) begin
+				if (rd_en_d2[j])
+					speed[j] <= xxx;
+			end
+		end
+	endgenerate
+	reg rd_en_final;
+	reg [C_BRAM_ADDR_WIDTH-1:0] rd_addr_final;
+	generate
+		for (i = 0; i < C_BRAM_ADDR_WIDTH; i = i+1) begin: single_addr_bit
+			always @ (posedge clk) begin
+				rd_en_final <= rd_en;
+				rd_addr_final[i] <= (rd_en & rd_addr_conv[i]);
+			end
+		end
+	endgenerate
+	always @ (posedge clk) begin
+		rd_en_d1 <= rd_en;
+		rd_en_d2 <= rd_en_d1;
+	end
+
+	localparam integer IDLE = 2'b00;
+	localparam integer ACCE = 2'b01;
+	localparam integer KEEP = 2'b11;
+	localparam integer DEAC = 2'b10;
+
+	generate
+		for (i = 0; i < C_MOTOR_NBR; i = i+1) begin: single_motor_logic
+			wire clk_pulse;
+			assign clk_pulse = clk_en[i];
+			/// start_pulse
+			reg start_d1;
+			reg start_pulse;
+			always @ (posedge clk) begin
+				if (resetn == 0)
+					start_d1 <= 0;
+				else
+					start_d1 <= i_start;
+			end
+			always @ (posedge clk) begin
+				if (resetn == 1'b0)
+					start_pulse <= 0;
+				else if (start_pulse && clk_pulse)
+					start_pulse <= 0;
+				else if (i_start && ~start_d1 && (motor_state == IDLE))
+					start_pulse <= 1;
+			end
+			always @ (posedge clk) begin
+				if (resetn == 1'b0)
+					rd_en[i] <= 0;
+				else if (rd_en[i])
+				 	rd_en[i] <= 0;
+				else if ((motor_state == IDLE) && start_pulse && clk_pulse)
+					rd_en[i] <= 1;
+			end
+
+			reg [C_SPEED_DATA_WIDTH-1:0]	pulse_width;	/// represent speed
+			reg [C_STEP_NUMBER_WIDTH-1:0]	remain_step;
+			reg[1:0] motor_state;
+			reg[1:0] next_state;
+			always @ (posedge clk) begin
+				if (resetn == 1'b0)
+					motor_state <= IDLE;
+				else begin
+					case (motor_state)
+					IDLE: begin
+						if (start_pulse) begin
+							pulse_width <= i_velocity;
+							remain_step <= i_step_nbr;
+							next_state <= ACCE;
+						end
+					end
+					endcase
+				end
+			end
+		end
+	endgenerate
 
 endmodule
