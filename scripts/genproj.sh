93c01dbb9ee2562e53d6b89d6272d72855a4f3cb
Yi Qingliang <niqingliang2003@gmail.com>
2017-11-20 14:29:52 +0800

    update gneproj.sh
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/genproj.sh
+++ scripts/genproj.sh
@@ -10,4 +10,6 @@ ${VIVADO_BIN} \
 	-nojournal -nolog \
 	-mode batch \
 	-source ${dir_main}/scripts/aux/genall.tcl \
-	-tclargs "${dir_main}" ${1+$@}
+	-tclargs "${dir_main}" ${1+$@} \
+
+#	|grep -v "^#"|grep -v "^INFO"|grep -v "19-3899"
