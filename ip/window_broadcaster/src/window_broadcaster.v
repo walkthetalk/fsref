07c32ca01264e1cb88f7bfea3a2ac053198359cb
Yi Qingliang <niqingliang2003@gmail.com>
2017-12-02 10:21:49 +0800

    window_broadcaster: reimplement
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- ip/window_broadcaster/src/window_broadcaster.v
+++ ip/window_broadcaster/src/window_broadcaster.v
@@ -54,54 +54,40 @@ module window_broadcaster #
 	output wire [C_HBITS-1 : 0] m7_top,
 	output wire [C_HBITS-1 : 0] m7_height
 );
+	localparam integer C_MAX_OUT = 8;
+	wire [C_WBITS-1 : 0] m_left  [C_MAX_OUT-1:0];
+	wire [C_WBITS-1 : 0] m_width [C_MAX_OUT-1:0];
+	wire [C_HBITS-1 : 0] m_top   [C_MAX_OUT-1:0];
+	wire [C_HBITS-1 : 0] m_height[C_MAX_OUT-1:0];
+`define ASSIGN_SINGLE_O(i) \
+	assign m``i``_left   = m_left  [i]; \
+	assign m``i``_width  = m_width [i]; \
+	assign m``i``_top    = m_top   [i]; \
+	assign m``i``_height = m_height[i];
+
+	`ASSIGN_SINGLE_O(0)
+	`ASSIGN_SINGLE_O(1)
+	`ASSIGN_SINGLE_O(2)
+	`ASSIGN_SINGLE_O(3)
+	`ASSIGN_SINGLE_O(4)
+	`ASSIGN_SINGLE_O(5)
+	`ASSIGN_SINGLE_O(6)
+	`ASSIGN_SINGLE_O(7)
 	generate
-		if (C_MASTER_NUM > 0) begin
-			assign m0_left   = s_left;
-			assign m0_top    = s_top;
-			assign m0_width  = s_width;
-			assign m0_height = s_height;
-		end
-		if (C_MASTER_NUM > 1) begin
-			assign m1_left   = s_left;
-			assign m1_top    = s_top;
-			assign m1_width  = s_width;
-			assign m1_height = s_height;
-		end
-		if (C_MASTER_NUM > 2) begin
-			assign m2_left   = s_left;
-			assign m2_top    = s_top;
-			assign m2_width  = s_width;
-			assign m2_height = s_height;
-		end
-		if (C_MASTER_NUM > 3) begin
-			assign m3_left   = s_left;
-			assign m3_top    = s_top;
-			assign m3_width  = s_width;
-			assign m3_height = s_height;
-		end
-		if (C_MASTER_NUM > 4) begin
-			assign m4_left   = s_left;
-			assign m4_top    = s_top;
-			assign m4_width  = s_width;
-			assign m4_height = s_height;
-		end
-		if (C_MASTER_NUM > 5) begin
-			assign m5_left   = s_left;
-			assign m5_top    = s_top;
-			assign m5_width  = s_width;
-			assign m5_height = s_height;
-		end
-		if (C_MASTER_NUM > 6) begin
-			assign m6_left   = s_left;
-			assign m6_top    = s_top;
-			assign m6_width  = s_width;
-			assign m6_height = s_height;
-		end
-		if (C_MASTER_NUM > 7) begin
-			assign m7_left   = s_left;
-			assign m7_top    = s_top;
-			assign m7_width  = s_width;
-			assign m7_height = s_height;
+		genvar i;
+		for (i=0; i < C_MAX_OUT; i = i+1) begin: single_output
+			if (i < C_MASTER_NUM) begin
+				assign m_left  [i] = s_left  ;
+				assign m_top   [i] = s_top   ;
+				assign m_width [i] = s_width ;
+				assign m_height[i] = s_height;
+			end
+			else begin
+				assign m_left  [i] = 0;
+				assign m_top   [i] = 0;
+				assign m_width [i] = 0;
+				assign m_height[i] = 0;
+			end
 		end
 	endgenerate
 
