f5e406095b6b190d81fa2429f8f02f7396aee728
Yi Qingliang <niqingliang2003@gmail.com>
2018-04-25 14:12:55 +0800

    add new interface: fsa_ctl
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

---
+++ ip/fsa_ctl/generate.tcl
@@ -0,0 +1,50 @@
+set origin_dir [lindex $argv 0]
+set ip_dir [file dirname $argv0]
+
+source $origin_dir/scripts/aux/util.tcl
+
+set bus_name fsa_ctl
+set abs_name fsa_ctl_rtl
+
+set abs_file ${ip_dir}/${abs_name}.xml
+set bus_file ${ip_dir}/${bus_name}.xml
+
+ipx::create_abstraction_definition $VENDOR interface ${abs_name} 1.0
+ipx::create_bus_definition $VENDOR interface ${bus_name} 1.0
+
+pip_set_prop [ipx::current_busabs] [subst {
+	xml_file_name $abs_file
+	bus_type_vlnv $VENDOR:interface:$bus_name:1.0
+}]
+
+pip_add_bus_abstraction_port [ipx::current_busabs] REF_DATA {
+	default_value 0
+	master_presence required
+	master_direction out
+	slave_presence required
+	is_data true
+}
+
+pip_add_bus_abstraction_port [ipx::current_busabs] LEFT_VERTEX {
+	default_value 0
+	master_presence required
+	master_direction in
+	slave_presence required
+	is_data true
+}
+
+pip_add_bus_abstraction_port [ipx::current_busabs] RIGHT_VERTEX {
+	default_value 0
+	master_presence required
+	master_direction in
+	slave_presence required
+	is_data true
+}
+
+ipx::save_abstraction_definition [ipx::current_busabs]
+
+pip_set_prop [ipx::current_busdef] [subst {
+	xml_file_name $bus_file
+}]
+
+ipx::save_bus_definition [ipx::current_busdef]
