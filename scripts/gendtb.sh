833f8a6a43f4ade85ba4e71f9188d910f942d44d
Yi Qingliang <niqingliang2003@gmail.com>
2019-09-28 23:37:36 +0800

    gendtb: generate ps7init/dts for yocto
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/gendtb.sh
+++ scripts/gendtb.sh
@@ -9,17 +9,22 @@ if [ ! -d "${out_dir}" ]; then
 	mkdir ${out_dir}
 fi
 
-tmp_dir="${out_dir}/dts"
-if [ ! -d "${tmp_dir}" ]; then
-	mkdir ${tmp_dir}
+ps7init_dir="${out_dir}/ps7init"
+if [ ! -d "${ps7init_dir}" ]; then
+	mkdir ${ps7init_dir}
 fi
 
-dtxdir="${dir_main}/../device-tree-xlnx/"
-if [ ! -d "${dtxdir}" ]; then
-	echo "ERROR: please clone device-tree-xlnx first!"
-	echo "HINT:  git cloen https://github.com/Xilinx/device-tree-xlnx.git"
-	exit 1
+dts_dir="${out_dir}/dts"
+if [ ! -d "${dts_dir}" ]; then
+	mkdir ${dts_dir}
 fi
+
+if [ -d "$1" ]; then
+	dtxdir="$1"
+else
+	dtxdir="${dir_main}/../device-tree-xlnx/"
+fi
+echo "HINT:  git clone https://github.com/Xilinx/device-tree-xlnx.git"
 
 repo_dt="`readlink -fe "${dtxdir}"`"
 
@@ -28,11 +33,11 @@ source ${dir_main}/scripts/setenv
 ${HSI_BIN} \
 	-nojournal -nolog \
 	-mode batch \
-	-source ${dir_main}/scripts/aux/gendts.tcl \
-	-tclargs "${dir_main}" "${repo_dt}" "${tmp_dir}"
+	-source ${dir_main}/scripts/aux/gen_ps7init_dts.tcl \
+	-tclargs "${dir_main}" "${repo_dt}" "${ps7init_dir}" "${dts_dir}"
 
 command -v dtc >/dev/null 2>&1 || { echo >&2 "ERROR: require dtc but not installed."; exit 1; }
 dtc -I dts -O dtb -R 8 -p 0x3000 \
-	-i ${tmp_dir} \
+	-i ${dts_dir} \
 	-o ${out_dir}/devicetree.dtb \
 	${dir_main}/scripts/dtspatch/wrapper.dts
