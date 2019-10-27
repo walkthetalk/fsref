3f800255672096df520f784391191c101f4bad56
Yi Qingliang <niqingliang2003@gmail.com>
2019-10-27 22:45:19 +0800

    kernel config: add fpga manager
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/gen_kernelpatch.sh
+++ scripts/gen_kernelpatch.sh
@@ -8,13 +8,17 @@ src_dir="${dir_main}/scripts/kernelpatch"
 out_dir="${dir_main}/output"
 tmp_dir="`mktemp -p /tmp -d fsref.XXXXX`"
 
+kernel_dir=${tmp_dir}/linux
+mkdir ${kernel_dir}
+
 patch_name="add_fsref.patch"
 
 file_list=(
 	"arch/arm/configs/fsref_defconfig"
+	"arch/arm/boot/dts/zynq-fsref.dts"
 )
 
-cd ${tmp_dir}
+cd ${kernel_dir}
 quilt new ${patch_name}
 for i in ${file_list[@]}; do
 	idir=`dirname $i`
@@ -25,6 +29,6 @@ for i in ${file_list[@]}; do
 done
 quilt refresh
 
-cp ${tmp_dir}/patches/${patch_name} ${out_dir}
+cp ${kernel_dir}/patches/${patch_name} ${out_dir}
 
 rm -rf ${tmp_dir}
