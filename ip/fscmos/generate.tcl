656aba4baee5218825743887c8b33f675dc30db2
Yi Qingliang <niqingliang2003@gmail.com>
2019-11-03 19:16:14 +0800

    fscmos: add option for in/out data width
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- ip/fscmos/generate.tcl
+++ ip/fscmos/generate.tcl
@@ -11,9 +11,24 @@ pip_add_bus_if $core vid_io_out {
 	VSYNC vid_vsync
 }
 
-pip_add_usr_par $core {C_DATA_WIDTH} {
-	display_name {Data Width}
-	tooltip {DATA WIDTH}
+pip_add_usr_par $core {C_IN_WIDTH} {
+	display_name {IN Width}
+	tooltip {IN WIDTH}
+	widget {comboBox}
+} {
+	value_resolve_type user
+	value 8
+	value_format long
+	value_validation_type list
+	value_validation_list {6 8 10 12}
+} {
+	value 8
+	value_format long
+}
+
+pip_add_usr_par $core {C_OUT_WIDTH} {
+	display_name {OUT Width}
+	tooltip {OUT WIDTH}
 	widget {comboBox}
 } {
 	value_resolve_type user
--- ip/fscmos/src/fscmos.v
+++ ip/fscmos/src/fscmos.v
@@ -21,27 +21,36 @@
 module fscmos #
 (
 	// Users to add parameters here
-	parameter integer C_DATA_WIDTH	= 8
+	parameter integer C_IN_WIDTH	= 8,
+	parameter integer C_OUT_WIDTH	= 8
 )
 (
 	input wire		cmos_pclk,
 
 	input wire		cmos_vsync,
 	input wire		cmos_href,
-	input wire [C_DATA_WIDTH-1:0]	cmos_data,
+	input wire [C_IN_WIDTH-1:0]	cmos_data,
 
 	output wire		vid_active_video,
-	output wire[C_DATA_WIDTH-1:0]	vid_data,
+	output wire[C_OUT_WIDTH-1:0]	vid_data,
 	output wire		vid_hblank,
 	output wire		vid_hsync,
 	output wire		vid_vblank,
 	output wire		vid_vsync
 );
 	assign vid_active_video= (cmos_href && ~cmos_vsync);
-	assign vid_data = cmos_data;
+
 	assign vid_hblank = ~cmos_href;
 	assign vid_hsync = vid_hblank;
 
 	assign vid_vsync = vid_vblank;
 	assign vid_vblank = ~cmos_vsync;
+generate
+if (C_IN_WIDTH < C_OUT_WIDTH) begin: in_lt_out
+	assign vid_data = {cmos_data, {(C_OUT_WIDTH - C_IN_WIDTH){1'b0}}};
+end
+else begin: in_ge_out
+	assign vid_data = cmos_data[C_IN_WIDTH-1:C_IN_WIDTH-C_OUT_WIDTH];
+end
+endgenerate
 endmodule
