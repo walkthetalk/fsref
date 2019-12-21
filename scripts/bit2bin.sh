43e618eef61c35f93f0e5778bd03173ee9eb5631
Yi Qingliang <niqingliang2003@gmail.com>
2019-12-21 14:47:01 +0800

    add comment for bit2bin
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/bit2bin.sh
+++ scripts/bit2bin.sh
@@ -18,3 +18,10 @@ SRC_FILE="${dir_main}/scripts/bit2bin/zynq_bit2bin.c"
 gcc ${SRC_FILE} -o ${out_dir}/bit2bin
 
 ${out_dir}/bit2bin ${dir_main}/fsref.runs/impl_1/${BDNAME}_wrapper.bit ${out_dir}/system.bit.bin
+
+echo "
+#Load the Bitstream
+	mkdir -p /lib/firmware
+	cp /media/design_1_wrapper.bit.bin /lib/firmware/
+	echo design_1_wrapper.bit.bin > /sys/class/fpga_manager/fpga0/firmware
+"
